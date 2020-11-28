87b68d98a855f105999f209264431b1f5d0b8f93
Helena Magaldi <helenamagaldi@gmail.com>
2020-11-28 17:23:15 -0300

    adding new project: Text Game

---
+++ Text Game/main.py
@@ -0,0 +1,47 @@
+# map
+
+locations = [
+    "Pat's",
+    "Karla's",
+    "Tata's",
+    "Mom's",
+    "Nona's",
+]
+
+location_benesses = { 
+    "Pat's": [
+        "Gabriel",
+        "Bilbo",
+        "Thorin",
+        "Francisca",
+    ],
+    "Karla's": [
+        "Nero",
+        "Martin",
+        "Naria",
+        "Frederico",
+    ],
+    "Tata's": [
+        "Miners", 
+    ],
+    "Mom's": [
+        "Mika",
+        "Chanel",
+        "Guinness",
+        "Mary",
+    ],
+    "Nona's": [
+        "Frederico",
+        "Francisco",
+        "Preta"
+    ],
+
+}
+
+location_descriptions = {
+    "Pat's": "Gordinha's house.",
+    "Karla's": "Mig's house.",
+    "Tata's": "Nena's house",
+    "Mom's": "Mama's house",
+    "Nona's": "Granny's house"
+}
