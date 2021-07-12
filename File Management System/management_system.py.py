6daaefd8526cc5e211654687c8f638ce0d8f6fe5
Helena Magaldi <helenamagaldi@gmail.com>
2021-07-12 17:25:45 -0300

    fix overall repository structure

---
+++ File Management System/management_system.py.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os 
+
+
+# In[6]:
+
+
+folderpath = r"/Users/helenamagaldi/Dropbox/My Mac (MacBook Pro de Helena)/Downloads"
+os.chdir(folderpath)
+
+
+# In[7]:
+
+
+os.getcwd()
+
+
+# In[8]:
+
+
+os.listdir()
+
+
+# In[ ]:
+
+
+
+
