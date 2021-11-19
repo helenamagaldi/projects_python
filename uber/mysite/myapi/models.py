0e59cd573a46c473aee61b1d28c24e680af83642
Helena Magaldi <helenamagaldi@gmail.com>
2021-11-19 16:28:23 -0300

    delete late technical projects

--- uber/mysite/myapi/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Hero(models.Model):
-    name = models.CharField(max_length=60)
-    alias = models.CharField(max_length=60)
-    
-    def __str__(self):
-        return self.name
--- uber/mysite/myapi/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import render
-
-from rest_framework import viewsets
-from .serializers import HeroSerializer
-from .models import Hero
-
-class HeroViewSet(viewsets.ModelViewSet):
-    queryset = Hero.objects.all().order_by('name')
-    serializer_class = HeroSerializer
