da377ca2d7adcc69c0b67fef5aaa2e2a51e62b5d
Helena Magaldi <helenamagaldi@gmail.com>
2021-11-23 11:34:32 -0300

    feat add log in button for authentification

--- new_django_project/blog/blog/urls.py
+++ new_django_project/blog/blog/urls.py
@@ -5,5 +5,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls'),)
+    path('', include('api.urls'),),
+    path('api-auth/', include('rest_framework.urls')),
 ]
