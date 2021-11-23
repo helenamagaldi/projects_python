4f138965428d1a4cee3bf0b64675e7ae95e5faa1
Helena Magaldi <helenamagaldi@gmail.com>
2021-11-23 11:35:55 -0300

    feat completing endpoints for Post API: add URL patterns

--- new_django_project/blog/api/urls.py
+++ new_django_project/blog/api/urls.py
@@ -7,6 +7,8 @@ from api import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>', views.UserDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
